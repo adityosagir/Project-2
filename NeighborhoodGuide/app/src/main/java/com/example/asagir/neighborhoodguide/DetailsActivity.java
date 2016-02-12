0ace3b199fc4f3eccde6efe5ca7d57555ed3aa55
adityosagir <adityo.sagir@gmail.com>
2016-02-12 00:32:23 -0500

    Images uploaded

--- NeighborhoodGuide/app/src/main/java/com/example/asagir/neighborhoodguide/DetailsActivity.java
+++ NeighborhoodGuide/app/src/main/java/com/example/asagir/neighborhoodguide/DetailsActivity.java
@@ -2,8 +2,11 @@ package com.example.asagir.neighborhoodguide;
 
 import android.content.Intent;
 import android.content.res.ColorStateList;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -59,19 +62,86 @@ public class DetailsActivity extends AppCompatActivity {
 
             switch (id) {
                 case 1:
-                    restaurantTopImage.setImageResource(R.drawable.crescentfood2);
-                    restaurantLeftImage.setImageResource(R.drawable.crescentoutdoors);
-                    restaurantRightImage.setImageResource(R.drawable.crescentfood);
+//                    restaurantTopImage.setImageBitmap(
+//                            decodeSampledBitmapFromResource(getResources(), R.drawable.crescentfood2, 100, 100));
+                    restaurantTopImage.setImageResource(R.drawable.crescentfood);
+                    restaurantRightImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.crescentoutdoors, 100, 100));
+                    restaurantLeftImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.crescentfood2, 100, 100));
+                    break;
                 case 2:
-                    restaurantTopImage.setImageResource(R.drawable.muramenfood2);
-                    restaurantLeftImage.setImageResource(R.drawable.murameninside);
-                    restaurantRightImage.setImageResource(R.drawable.muramenfood);
-//                case "upload":
-//                    return android.R.drawable.ic_menu_upload;
-//                case "play":
-//                    return android.R.drawable.ic_media_play;
-//                default:
-//                    return 0;
+                    restaurantTopImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.muramenfood2, 100, 100));
+                    restaurantRightImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.muramenfood, 100, 100));
+                    restaurantLeftImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.murameninside, 100, 100));
+                    break;
+                case 3:
+                    restaurantTopImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.mwellslogo, 100, 100));
+                    restaurantRightImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.mwellsfood, 100, 100));
+                    restaurantLeftImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.mwellsinside, 100, 100));
+                    break;
+                case 4:
+                    restaurantTopImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.kavalalogo, 100, 100));
+                    restaurantRightImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.kavalafood, 100, 100));
+                    restaurantLeftImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.kavalainside, 100, 100));
+                    break;
+                case 5:
+                    restaurantTopImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.houseofthailogo, 100, 100));
+                    restaurantRightImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.houseofthaifood, 100, 100));
+                    restaurantLeftImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.houseofthaiinside, 100, 100));
+                    break;
+                case 6:
+                    restaurantTopImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.tasteofbengaloutdoors, 100, 100));
+                    restaurantRightImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.tasteofbengalfood, 100, 100));
+                    restaurantLeftImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.tasteofbengalfood2, 100, 100));
+                    break;
+                case 7:
+                    restaurantTopImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.malaguetalogo, 100, 100));
+                    restaurantRightImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.malaguetafood, 100, 100));
+                    restaurantLeftImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.malaguetainside, 100, 100));
+                    break;
+                case 8:
+                    restaurantTopImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.goodtasteoutside, 100, 100));
+                    restaurantRightImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.goodtastefood, 100, 100));
+                    restaurantLeftImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.goodtastefood2, 100, 100));
+                    break;
+                case 9:
+                    restaurantTopImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.manettaoutside, 100, 100));
+                    restaurantRightImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.manettafood, 100, 100));
+                    restaurantLeftImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.manettainside, 100, 100));
+                    break;
+                case 10:
+                    restaurantTopImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.casaenriqueteam, 100, 100));
+                    restaurantRightImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.casaenriquefood, 100, 100));
+                    restaurantLeftImage.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(), R.drawable.casaenriquefood2, 100, 100));
+                    break;
             }
 
             actionBarSetup(name);
@@ -80,7 +150,7 @@ public class DetailsActivity extends AppCompatActivity {
 
             if (mHelper.checkFavorite(id) == 1) {
                 mFavFlag = true;
-                buttonFav.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+                buttonFav.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#43A047")));
             } else {
                 mFavFlag = false;
                 buttonFav.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#40BFFF")));
@@ -100,7 +170,7 @@ public class DetailsActivity extends AppCompatActivity {
                         SQLiteDatabase db = mHelper.getWritableDatabase();
                         db.execSQL("UPDATE RestaurantDetails SET Favorite = 1 WHERE _id = " + id);
                         Toast.makeText(DetailsActivity.this, mHelper.getNameById(id) + " was added", Toast.LENGTH_SHORT).show();
-                        buttonFav.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+                        buttonFav.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#43A047")));
                         mFavFlag = true;
                     }
 
@@ -116,5 +186,46 @@ public class DetailsActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle(title);
     }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+
+    }
+
 
 }
